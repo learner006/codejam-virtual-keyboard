2ec61a344b3a45ada78a18d9600a14f951245df4
learner006 <learner06@mail.ru>
2019-11-07 15:45:52 +0300

    docs: create keyboard layouts using JS arrays
    There are English and Russian layouts.

---
+++ kdb_chars_layout/key_widths.js
@@ -0,0 +1,7 @@
+const KBKeyWidths = [
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1.7], // Esc 14x1 Bksp
+  [2,1,1,1,1,1,1,1,1,1,1,1,1,1,1],     // Tab 14x1 Del
+  [2.6,1,1,1,1,1,1,1,1,1,1,1,1,2.5],   // Caps 11x1 Enter
+  [3.2,1,1,1,1,1,1,1,1,1,1,1,1,2.2],   // LShift 11x1 RShift
+  [1.7,1,1,5.5,1,1.7,1,1,1,1]    // LCtrl, Win, LAlt, Space, RAlt, Context menu, RCtrl, L, B, U, Fn
+];
